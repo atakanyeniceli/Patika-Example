321618b84c69addb77c37dd81de58b05005ffa02
atakanyeniceli <48385755+atakanyeniceli@users.noreply.github.com>
2021-11-14 15:51:28 +0300

    Examples

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,6 +1,10 @@
-import slugify from "slugify";
-import Topla,{sub,mul,div} from "./myModule.js"
+import slugify from 'slugify';
+import Topla,{sub,mul,div} from './myModule.js';
+import fetch from 'node-fetch';
 
 
 console.log(slugify("Hello Word"));
 console.log(Topla(1,2),sub(1,2),mul(2,2),div(5,0))
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(data => data.json())
+.then(userJson =>console.log(userJson))
