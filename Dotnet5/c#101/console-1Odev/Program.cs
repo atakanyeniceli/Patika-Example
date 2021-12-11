55973ceeb1ad6700c60637a8086f58f5df67c5f2
example <48385755+atakanyeniceli@users.noreply.github.com>
2021-12-11 17:37:58 +0300

    Examples

---
+++ Dotnet5/c#101/console-1Odev/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace console_1Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Girmek İstediğiniz Sayi Adedini Giriniz.");
+            int count=Convert.ToInt32(Console.ReadLine());
+            List<int> myArray=new List<int>();
+            for(int i=0; i <count;){
+                Console.WriteLine("Lütfen Pozitif bir sayi giriniz.");
+                try{
+                    int res=Convert.ToInt32(Console.ReadLine());
+                    if(res>0){
+                        myArray.Add(res);
+                        i++;
+                    }
+                }
+                catch(Exception e){}
+            }
+            myArray.ForEach(item => Console.Write(item%2 == 0 ? item.ToString()+'-' :null));
+        }
+    }   
+}
