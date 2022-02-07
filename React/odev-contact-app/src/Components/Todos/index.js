fe7e68ba508a43325c3c01c5c1e293a4a2760cfc
atakanyeniceli <48385755+atakanyeniceli@users.noreply.github.com>
2022-02-07 21:51:11 +0300

    Examples

--- React/odev-contact-app/src/Components/Todos/index.js
+++ React/odev-contact-app/src/Components/Todos/index.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import './myStyle.css'
 
 function Todos() {
@@ -15,19 +15,25 @@ function Todos() {
 
     const addTodo = (e) => {
         e.preventDefault();
-        let inputValue=e.target.querySelector('#title').value
-        if (inputValue === '') return false;
-        setList([...list, { status: false, title: inputValue }])
-        e.target.querySelector('#title').value=''
-    }
-    
-    const checkedChange=()=>{
+        const resValue = e.target.querySelector('#todoInput').value;
+        const valueCheck = list.map((item) => { return item.title }).includes(resValue);
 
+        if (resValue == '' || valueCheck) return false;
+        setList([...list, { status: false, title: resValue }]);
     }
 
-    useEffect(()=>{
+    const onChangeChecked = (e) => {
+        const res = list.findIndex((item) => item.title == e.target.title)
+        list[res] = { status: (e.target.checked), title: e.target.title }
+        setList([...list])
+    }
 
-    });
+    const todoDelete = (e) => {
+        const resTitle = e.target.title
+        const res = list.filter((item) => item.title != resTitle)
+        setList([...res])
+    }
+
     return (
         <div className='mainDiv'>
             <div><h1>Todos</h1></div>
@@ -35,33 +41,31 @@ function Todos() {
 
                 <div className='shadow header'>
                     <label id='arrow'></label>
-                    <input id='title'className='shadow' placeholder='What needs to be done?' />
+                    <input id='todoInput' className='shadow' placeholder='What needs to be done?' />
                 </div>
-
-                <div>
-                    <ul>
-                        {list.map(
-                            (item, key) =>
-                                <li key={key} className='shadow'>
-                                    <div className='list'>
-                                        <input type='checkbox'  defaultChecked={item.status}/>
-                                        <label>{item.title}</label>
-                                        <button></button>
-                                    </div>
-                                </li>
-                            )
-                        }
-                    </ul>
-                </div>
-
-                <div className='footer'>
-                    <span >{list.length} items left</span>
-                    <span ><a href="">All</a></span>
-                    <span ><a href="">Active</a></span>
-                    <span ><a href="">Completed</a></span>
-                </div>
-
             </form>
+            <div>
+                <ul>
+                    {list.map((item, key) =>
+                        <li key={key} className='shadow'>
+                            <div className='list'>
+                                <input type='checkbox' title={item.title} onChange={onChangeChecked} defaultChecked={item.status} />
+                                <label> {item.title} </label>
+                                <button title={item.title} onClick={todoDelete}></button>
+                            </div>
+                        </li>
+                    )
+                    }
+                </ul>
+            </div>
+
+            <div className='footer'>
+                <span >{list.length} items left</span>
+                <span ><a >All</a></span>
+                <span ><a >Active</a></span>
+                <span ><a >Completed</a></span>
+            </div>
+
         </div>
     )
 }
