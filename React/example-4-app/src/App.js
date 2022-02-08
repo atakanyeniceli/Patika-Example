7f64f42ac36b398715c4952e613110a358a93a86
atakanyeniceli <48385755+atakanyeniceli@users.noreply.github.com>
2022-02-08 19:46:47 +0300

    Examples

---
+++ React/example-4-app/src/App.js
@@ -0,0 +1,15 @@
+import A from './Components/A';
+import B from './Components/B';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <A></A>
+      <B></B>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/example-4-app/src/Components/A/index.js
@@ -0,0 +1,7 @@
+import styles from './styles.module.css'
+
+function A() {
+  return <div className={styles.title}>A</div>;
+}
+
+export default A;
