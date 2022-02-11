b296950bd6837901ce4027ef20db29ff5b24cd73
atakanyeniceli <48385755+atakanyeniceli@users.noreply.github.com>
2022-02-11 11:05:12 +0300

    Examples

--- React/example-4-app/src/App.js
+++ React/example-4-app/src/App.js
@@ -1,14 +1,57 @@
 import A from './Components/A';
 import B from './Components/B';
+import Users from './Components/Users'
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <A></A>
-      <B></B>
+    <Main></Main>
+  );
+}
+function Main() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to React Router!</h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
