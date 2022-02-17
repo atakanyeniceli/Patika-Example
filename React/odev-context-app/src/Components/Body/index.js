6594fe449d9218c2953275078e6713c8448f162e
example <48385755+atakanyeniceli@users.noreply.github.com>
2022-02-17 23:56:02 +0300

    Examples

--- React/odev-context-app/src/Components/Body/index.js
+++ React/odev-context-app/src/Components/Body/index.js
@@ -1,22 +1,37 @@
 import { useContext } from 'react'
 import DataContext from '../../Context/DataContext'
-import moment from 'moment'
+import './css/weather-icons.css'
+import './css/weather-icons-wind.css'
 
 function Body() {
     const { data } = useContext(DataContext)
     return (
-        <div className='card'>
-            {data.map((item, index) => <div key={index}>
-                <div>{moment(item.dateTime).format('dddd')}</div>
-                <div><img src={item.icon} alt={item.description}></img></div>
+        <div className='main'>
+            {/* {data.map((item, index) => <div className='card' key={index}>
+                <span>{item.dateTime}</span>
+
+                <div><img src={item.icon.url} alt={item.icon.description}></img></div>
+                <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <div>
+                        < i className='wi wi-humidity' /> <span style={{ marginRight: '5px' }}>{item.humidity}</span>
+                    </div>
+                    <div>
+                        <i className={`wi wi-wind towards-${item.windDeg}-deg`} /> <span style={{ marginRight: '5px' }}>{item.windSpeed}metre/sec</span>
+                    </div>
+                </div>
+
                 <div>Day={item.temp.day}</div>
                 <div>Evenig={item.temp.eve}</div>
                 <div>Max={item.temp.max}</div>
                 <div>Min={item.temp.min}</div>
-            </div>)}
+                <div>humi={item.humidity}</div>
+
+            </div>)} */}
+            <div className='card'>
+                
+            </div>
         </div>
     )
 }
 
 export default Body
-// dateTime: timeConvert(day.dt), temp: { day: day.temp.day, eve: day.temp.eve, max: day.temp.max, min: day.temp.min, night: day.temp.night
--- React/odev-context-app/src/Components/Header/index.js
+++ React/odev-context-app/src/Components/Header/index.js
@@ -3,6 +3,7 @@ import { cityDetails } from '../Data/Data'
 import { useContext } from 'react'
 import axios from 'axios'
 import DataContext from '../../Context/DataContext'
+import moment from 'moment'
 
 const getData = (coordinate, setData) => {
     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=minutely,hourly&&units=metric&appid=7a905d1cfe10e6935c9931158e375812`)
@@ -18,27 +19,31 @@ const dataProcess = (resData) => {
         resArray.push(
             {
                 dateTime: timeConvert(day.dt),
+                humidity: day.humidity,
+                windDeg: day.wind_deg,
+                windSpeed: day.wind_speed,
                 temp: { day: day.temp.day, eve: day.temp.eve, max: day.temp.max, min: day.temp.min, night: day.temp.night },
-                icon: getIcon(day.weather[0]),
-                description: day.weather[0].description
+                feelsLike: { day: day.feels_like.day, night: day.feels_like.night, eve: day.feels_like.eve, morn: day.feels_like.morn },
+                icon: { url: getIcon(day.weather[0]), description: day.weather[0].description }
             })
     })
     return resArray
 }
+
 const getIcon = (iconCode) => {
     return `https://openweathermap.org/img/wn/${iconCode.icon}@2x.png`
 }
 
 const timeConvert = (timespan) => {
     const date = new Date(timespan * 1000)
-    return date
+    return moment(date).format('dddd')
 }
 
 
 function Header() {
     let { setData } = useContext(DataContext)
     return (
-        <div>
+        <div style={{display:'flex',justifyContent:'center',marginBottom:'30px'}}> 
             <label htmlFor='cities'>Choose a City:</label>
             <select name="city" id="cities" onChange={(res) => { getData(JSON.parse(res.target.value), setData) }}>
                 {cityDetails.map((city) => <option key={city.id} value={`{"lat":${city.latitude},"lon":${city.longitude}}`} > {city.name}</option>)}
