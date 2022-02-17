9c8071dbaf8974dc9d0e71fb6dbb2ad439c5adf9
atakanyeniceli <48385755+atakanyeniceli@users.noreply.github.com>
2022-02-17 10:01:51 +0300

    Examples

--- React/odev-context-app/src/App.js
+++ React/odev-context-app/src/App.js
@@ -1,5 +1,6 @@
 import Header from './Components/Header/';
 import Body from './Components/Body/';
+
 import './App.css';
 
 function App() {
--- React/odev-context-app/src/Components/Header/index.js
+++ React/odev-context-app/src/Components/Header/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { city } from '../Data/Data'
+import { cityDetails } from '../Data/Data'
+import axios from 'axios'
+
+const getData = (coordinate) => {
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=minutely,hourly&&units=metric&appid=7a905d1cfe10e6935c9931158e375812`)
+        .then((res) => timeConvert(res.data.current.dt))
+}
+const timeConvert = (timespan) => {
+    const date = new Date(timespan*1000)
+    console.log(date)
+}
 
 function Header() {
     return (
         <div>
-            <label htmlFor='citys'>Choose a City:</label>
-            <select name="city" id="citys">
-                {city}
+            <label htmlFor='cities'>Choose a City:</label>
+            <select name="city" id="cities" onChange={(res) => getData(JSON.parse(res.target.value))}>
+                {cityDetails.map((city) => <option key={city.id} value={`{"lat":${city.latitude},"lon":${city.longitude}}`} > {city.name}</option>)}
             </select>
-        </div>
+        </div >
     )
 }
 
